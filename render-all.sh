6e3362888a93a65c525b675889cb5cf213a20e22
Niklas Gawell <niklasgawell@Niklass-MBP.viasat.nds.com>
2020-12-13 15:52:27 +0100

    Music

--- render-all.sh
+++ render-all.sh
@@ -2,6 +2,8 @@
 
 #rm -rf render
 
+/Applications/Blender.app/Contents/MacOS/Blender -b pretext-1.blend -a
+/Applications/Blender.app/Contents/MacOS/Blender -b endtext-1.blend -a
 /Applications/Blender.app/Contents/MacOS/Blender -b scene-1.1.blend -S camera-1 -a -b
 /Applications/Blender.app/Contents/MacOS/Blender -b scene-1.1.blend -S camera-2 -a -b
 /Applications/Blender.app/Contents/MacOS/Blender -b scene-1.1.blend -S camera-3 -a -b
