ed77198f5181dfd5ccd2313938ca033af29d5c92
Niklas Gawell <niklasgawell@Niklass-MBP.viasat.nds.com>
2020-12-13 10:58:46 +0100

    Started editing

---
+++ render-all.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+#rm -rf render
+
+/Applications/Blender.app/Contents/MacOS/Blender -b scene-1.1.blend -S camera-1 -a -b
+/Applications/Blender.app/Contents/MacOS/Blender -b scene-1.1.blend -S camera-2 -a -b
+/Applications/Blender.app/Contents/MacOS/Blender -b scene-1.1.blend -S camera-3 -a -b
